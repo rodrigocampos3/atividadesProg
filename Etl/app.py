ed44e49670fd991a679f13c5ce9b9a3070d6dc9b
rodrigorodriguesb <rodrigo.rodrigues@sou.inteli.edu.br>
2023-11-14 23:39:28 -0300

    add db

--- Etl/app.py
+++ Etl/app.py
@@ -4,11 +4,28 @@ import pandas as pd
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+import sqlite3
 
 # Carrega as variáveis de ambiente a partir do arquivo .env
 load_dotenv()
 
 app = Flask(__name__)
+
+# Função para criar a tabela no banco de dados SQLite
+def create_table():
+    conn = sqlite3.connect('weather_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS weather (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ingestion_date TEXT,
+            weather_type TEXT,
+            weather_values TEXT,
+            usage TEXT
+        )
+    ''') 
+    conn.commit()
+    conn.close()
 
 @app.route('/extract_transform_load')
 def extract_transform_load():
@@ -22,21 +39,19 @@ def extract_transform_load():
     # 2. Transformar os dados
     date_ingestion = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     weather_type = weather_data['weather'][0]['main']
-    values = weather_data['main']
+    values = str(weather_data['main'])
     usage = 'OpenWeather API'
 
-    # 3. Criar DataFrame pandas
-    df = pd.DataFrame({
-        'Data da Ingestão': [date_ingestion],
-        'Tipo': [weather_type],
-        'Valores': [values],
-        'Uso': [usage]
-    })
-
-    # 4. Carregar os dados em uma tabela (usando um arquivo CSV)
-    df.to_csv('weather_data.csv', mode='a', header=not os.path.exists('weather_data.csv'), index=False)
+    # 3. Conectar ao banco de dados SQLite e inserir os dados
+    conn = sqlite3.connect('weather_data.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO weather (ingestion_date, weather_type, weather_values, usage) VALUES (?, ?, ?, ?)',
+                   (date_ingestion, weather_type, values, usage))
+    conn.commit()
+    conn.close()
 
     return jsonify({'message': 'ETL executado com sucesso!'})
 
 if __name__ == '__main__':
+    create_table()  # Cria a tabela quando o aplicativo é iniciado
     app.run(debug=True)
